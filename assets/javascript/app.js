e9c77267efc322d0e00676d7f406b4c63fd1d8b7
jmihalsky <jmihalsky@gmail.com>
2018-09-22 22:43:46 -0700

    final commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,8 +2,13 @@ var level = "level1";
 var correct = 0;
 var wrong = 0;
 var unaswr = 0;
+var t_correct = 0;
+var t_wrong = 0;
+var t_unaswr = 0;
 var timer_run = false;
 var lvl1_time = 20;
+var lvl2_time = 5;
+var lvl2_i = 0;
 
 
 var level1 = [
@@ -56,6 +61,60 @@ var level1 = [
         question: "What nationality is the 1990s pop group 'Ace of Base'? ",
         opt: ["Swedish","Norwegian","French","Danish"],
         aswr: 0
+    }
+
+];
+
+var level2 = [
+    {
+        question: "Insomniac and Dookie were 1990s albums by which American punk rock band?",
+        opt: ["Bloodhound Gang","Green Day","Blink 182","The Offspring"],
+        aswr: 1
+    },
+    {
+        question: "American singer, songwriter Mariah Carey had her first number one song of the 1990s with which song?",
+        opt: ["Vision of Love","All I Need","Glitter","Without You"],
+        aswr: 0
+    },
+    {
+        question: "What song is this line from? 'All five horizons revolved around the sun.'",
+        opt: ["Red Hot Chili Peppers","Soundgarden","Pearl Jam","Mother Love Bone"],
+        aswr: 2
+    },
+    {
+        question: "Who sang this: 'It's sittin' by the overcoat, the second shelf, the note she wrote.'",
+        opt: ["Smashmouth","Counting Crows","Backstreet Boys","Matchbox 20"],
+        aswr: 3
+    },
+    {
+        question: "Nirvana - In Utero (1993) Which was the first single released off of this album?",
+        opt: ["Smells Like Teen Spirt","Come As You Are","Heart Shaped Box","Pennyroyal Tea"],
+        aswr: 2
+    },
+    {
+        question: "What a girl wants, What a girl ____",
+        opt: ["has","has to have","wants","needs"],
+        aswr: 3
+    },
+    {
+        question: "Who sang 'Private Show'?",
+        opt: ["Pink","Jessica Simpson","Prince","Eden's Crush"],
+        aswr: 0
+    },
+    {
+        question: "What is the name of the first hit single by 3 Doors Down?",
+        opt: ["Rusty Cage","Kryptonite","Superman","Hunger Strike"],
+        aswr: 1
+    },
+    {
+        question: "Which Green Day album was released in 1994 with songs like Longview', 'Basket Case', and 'When I Come Around'?",
+        opt: ["American Idiot","Green Day","Dookie","Kerplunk"],
+        aswr: 2
+    },
+    {
+        question: "Name the classic rock band from Boston that came out with a hit album, and a trilogy of music videos for it.",
+        opt: ["J. Geils Band","Boston","Staind","Aerosmith"],
+        aswr: 3
     }
 
 ];
@@ -149,8 +208,31 @@ $("#submit").on("click",function() {
     }
     else if (level == "level2")
     {
-
+        
     }
+});
+
+$("#reset").on("click",function() {
+    correct = 0;
+    wrong = 0;
+    unaswr = 0;
+    lvl1_time = 20;
+    level="level1";
+    $(".ques-space").empty();
+    set_questions();
+    $("#submit").css("display","block");
+    $("#reset").css("display","none");
+});
+
+$("#lvl2").on("click",function() {
+    level="level2";
+    correct = 0;
+    wrong = 0;
+    unaswr = 0;
+    $(".ques-space").empty();
+    set_questions();
+    $("#submit").css("display","block");
+    $("#lvl2").css("display","none");
 });
 
 function lvl1_valid(){
@@ -159,7 +241,17 @@ function lvl1_valid(){
             answr_valid(i);
             
         }
-        score();
+    score();
+    if(correct >= 8)
+    {
+        $("#submit").css("display","none");
+        $("#lvl2").css("display","block");
+    }
+    else
+    {
+        $("#reset").css("display","block"); 
+        $("#submit").css("display","none");
+    }    
 }
 
 function answr_valid(x){
